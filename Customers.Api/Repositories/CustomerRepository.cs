dc390e553814708aa8d412d304aeb2b97b61d613
Maximiliano Sariego <maximiliano.sariego@end-game.com>
2023-05-04 10:22:45 +1200

    Add APIs

---
+++ Customers.Api/Repositories/CustomerRepository.cs
@@ -0,0 +1,31 @@
+﻿using Customers.Api.Contracts.Data;
+
+namespace Customers.Api.Repositories;
+
+public class CustomerRepository : ICustomerRepository
+{
+    public async Task<bool> CreateAsync(CustomerDto customer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<CustomerDto?> GetAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<CustomerDto>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> UpdateAsync(CustomerDto customer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> DeleteAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
